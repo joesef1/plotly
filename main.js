028247676650907dce6050f41710e6cc560c0b6c
jousef1 <yousefel560@gmail.com>
2023-03-03 10:24:44 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+window.onload = function() {
+  const myDiv = document.getElementById("myDiv");
+  console.log(myDiv);
+
+//
+  // var trace1 = {
+  //     x:['2020-10-04', '2021-11-04', '2023-12-04'],
+  //     y: [90, 40, 60],
+  //     type: 'scatter'
+  // };
+
+  // var data = [trace1];
+
+  // var layout = {
+  //     title: 'Scroll and Zoom by joe',
+  //     showlegend: false
+  // };
+
+  // Plotly.newPlot('myDiv', data, layout, {scrollZoom: true});
+  //
+}
+
+
